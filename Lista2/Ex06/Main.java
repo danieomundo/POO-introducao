776dbaa8ab9a92affb7215cd43bac71e597ac527
Dani <daniele.ddsp@gmail.com>
2022-04-17 00:41:40 -0300

    Create Main.java

---
+++ Lista2/Ex06/Main.java
@@ -0,0 +1,11 @@
+/*
+
+ENUNCIADO:
+
+Para implementar jogos com cartas, são necessárias classes para representar uma 
+carta individual e também um baralho. Sugira implementações para essas classes, 
+considerando:
+a.	Cartas - ás, 2 a 9, valete, dama e rei, dos naipes copas, espadas, ouros e paus
+b.	Baralho - conjunto de 52 cartas (13 cartas de cada naipe), em ordem ou embaralhado
+
+*/
