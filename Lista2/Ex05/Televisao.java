a9a45232b438e3eccc43d2df1c3b41e3d60208e0
Dani <daniele.ddsp@gmail.com>
2022-04-17 03:26:44 -0300

    Update Televisao.java

--- Lista2/Ex05/Televisao.java
+++ Lista2/Ex05/Televisao.java
@@ -7,13 +7,13 @@ public class Televisao {
     
     public void ligaTV(){     
         
-        System.out.println("Ligando TV......");
+        System.out.println("Ligando a TV...");
         
         status_tv = true;
         vol_tv = 3;
         num_canal = 4;
         
-        System.out.println("TV ligada!!!");
+        System.out.println("TV ligada.");
         System.out.println("Canal: " +num_canal);
         System.out.println("Volume: " +vol_tv);
         
@@ -21,38 +21,38 @@ public class Televisao {
     
     public void desligaTV() {
         
-     System.out.println("Desigando TV......");
+     System.out.println("Desligando a TV...");
      
      status_tv = false;
      num_canal=0;
      vol_tv=0;
      
-     System.out.println("TV desligada!!!");
+     System.out.println("TV desligada.");
         
     }
     
     public void mute(){
         silenciar_tv = true;
         vol_tv=-1;
-        System.out.println("TV MUTADA!");
+        System.out.println("Mute ON.");
     }
     
     public void unmute(){
         silenciar_tv = false;
         vol_tv = 3;
-        System.out.println("UNMUTE! Volume: " +vol_tv);
+        System.out.println("Mute OFF. \nVolume: " +vol_tv);
     }
     
     public void mudaCanal(int canal){
         
         if (vol_tv==0){
-            System.out.println("ERRO! TV ta desligada. Ligue primeiro.");
+            System.out.println("ERRO: TV desligada.");
         } else {
             if (canal>0 && canal<99) {
                 num_canal = canal;
                 System.out.println("Canal: "+canal);           
             } else {
-               System.out.println("Erro... canal invalido"); 
+               System.out.println("ERRO: Canal inválido."); 
             }
         }
     } 
@@ -60,12 +60,12 @@ public class Televisao {
   
     public void canalAnterior(){
         num_canal-=1;
-        System.out.println("Canal anterior:" +num_canal);
+        System.out.println("Canal anterior: " +num_canal);
     }
     
     public void proxCanal(){
         num_canal+=1;
-        System.out.println("Proximo canal: "+num_canal);
+        System.out.println("Próximo canal: "+num_canal);
     }
     
     public void aumentaVolume(){
@@ -74,10 +74,10 @@ public class Televisao {
             vol_tv+=1;
             System.out.println("Volume: " +vol_tv);
         } else {
-            System.out.println("Volume maximo!");
+            System.out.println("Volume máximo.");
         }
         if (vol_tv==0){
-        System.out.println("ERRO! TV ta desligada.");
+        System.out.println("ERRO: TV desligada.");
         }
     
     }
@@ -86,19 +86,19 @@ public class Televisao {
         if (vol_tv != 1){
             vol_tv -=1;
         } else {
-            System.out.println("Volume minimo!");
+            System.out.println("Volume mínimo.");
         }
         if (vol_tv==0){
-        System.out.println("ERRO! TV ta desligada.");
+        System.out.println("ERRO: TV desligada.");
         }
    
     }
     
     public void estadoTV(){
         if (status_tv == true) {
-            System.out.println("Estado: TV LIGADA!");
+            System.out.println("Estado atual: TV LIGADA!");
         } else {
-            System.out.println("Estado: TV DESLIGADA!");
+            System.out.println("Estado atual: TV DESLIGADA!");
         }
     }
              
