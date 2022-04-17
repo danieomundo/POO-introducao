8a25efd24daa392e0e1aecb7b9b6f276935e0a33
Dani <daniele.ddsp@gmail.com>
2022-04-16 21:20:52 -0300

    Create Main.java

---
+++ Lista2/Ex01/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    
+    public static void main (String[] args){
+ 
+        Contato contato = new Contato();
+        contato.nome = "Daniele";
+        contato.idade = 28;
+        contato.end = "Sao Domingos, Niteroi";
+        contato.telefone = 123456789;
+        
+        Agenda agenda = new Agenda();        
+        agenda.addContato(contato);
+       
+        System.out.println("Contato salvo: ");
+        agenda.imprimeContatos(contato);
+    
+    } 
+  
+}
