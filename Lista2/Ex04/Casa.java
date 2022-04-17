55391b584911f8cc04eb6539c437825689035f14
Dani <daniele.ddsp@gmail.com>
2022-04-17 03:15:09 -0300

    Update Casa.java
    mudancinha no código e testando novo jeito com for

--- Lista2/Ex04/Casa.java
+++ Lista2/Ex04/Casa.java
@@ -5,20 +5,24 @@ public class Casa {
     ArrayList<String> coresCasa = new ArrayList();
     ArrayList<Porta> portasCasa = new ArrayList();
 
-    public void addPorta(Porta porta){
+    public void addPorta(Porta porta) {
         portasCasa.add(porta);
+        System.out.println("Colocando uma porta na casa...Porta adicionada.");
     }
 
     public void pintaCasa(String cor){
         coresCasa.add(cor);
     }
-    public void imprimeCoresCasa(){
-        System.out.println(coresCasa);
-    }
     
-     /*public void quantasPortasEstaoAbertas(){
+     public void quantasPortasEstaoAbertas(){
+       
         int portaAberta = 0;
-        //arrumar metodo...........
-     }*/
-   
+
+        for(Porta porta : portasCasa){
+            if(porta.estadoPorta == true){
+                portaAberta++;
+            }
+        }
+        System.out.println("Quantidade de portas abertas: " + portaAberta);
+     }  
 }
